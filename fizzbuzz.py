fadf30e5d03e0e303ae3b7ccd2350699d06ac6bd
jonathan <hemepositive@gmail.com>
2014-06-13 17:45:22 -0400

    basic fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,15 @@
-# Write a program that prints the numbers from 1 to 100. 
+# The basic FizzBuzz
 
-for x in range(1,101):
-	print x
+def fizzbuzz():
+    for x in range(1, 101):
+    	#print "testing: {}".format(x)
+        if (x % 3 == 0) and (x % 5 == 0):
+            print "FizzBuzz"
+        elif x % 5 == 0:
+            print "Buzz"
+        elif x % 3 == 0:
+            print "Fizz"
+        else:
+            print x
+
+fizzbuzz()
