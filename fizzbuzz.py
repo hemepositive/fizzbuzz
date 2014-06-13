95be7b1efcd8ac841f39d3b094b69e0cf6ffbee5
jonathan <hemepositive@gmail.com>
2014-06-13 18:00:20 -0400

    added user input

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,21 @@
-# The basic FizzBuzz
+"""Refactor the code so that it uses a function, called `fizzbuzz()`, that takes a number as its argument. 
+Then use this argument as the upper limit of the range. So, instead of a range from 1 to 100, 
+the range starts at 1 and ends at the user-supplied number. Make sure to provide a 
+`if __name__ == '__main__':`."""
 
-def fizzbuzz():
-    for x in range(1, 101):
-    	#print "testing: {}".format(x)
-        if (x % 3 == 0) and (x % 5 == 0):
-            print "FizzBuzz"
-        elif x % 5 == 0:
-            print "Buzz"
-        elif x % 3 == 0:
-            print "Fizz"
-        else:
-            print x
 
-fizzbuzz()
+def fizzbuzz(n=100):
+	for x in range(1, n + 1):
+		if (x%3==0) and (x%5==0):
+			result = "Fizz Buzz"
+		elif (x%5==0):
+			result = "Buzz"
+		elif (x%3==0):
+			result = "Fizz"
+		else:
+			result = x
+		print result
+
+if __name__ ==  "__main__":
+    number = raw_input("Enter a number > ")
+    out = fizzbuzz(int(number))
