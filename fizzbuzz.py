6d9a571a9ab433db0d1d196d62c41bcf17cc4401
jonathan <hemepositive@gmail.com>
2014-06-18 05:12:01 -0400

    version 13; now 4 functions; new tests pass

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,10 @@
-"""Finally, you probably used the modulus operator, `%`,
-to to test if a number is a multiple of 3, 5, or 15. 
-Refactor your code so you are not using that operator.
-version 12"""
+"""Make sure you have 4 total functions that are named appropriately. 
+Add docstrings to each. Make sure all your tests pass. Then PUSH your code to Github. .
+version 13"""
 
-# Changed to use method other than modulo
-# I like string methods and they have not yet 
-# been required in advanced fizzbuzz
-# I also like my boolean tuple/dictionary method 
+# Oops I have > 4 functions; combinded 2 functions into validate_user_input
+# Adding docstrings
+
 def fizzbuzz(number=100):
     out = []
     words = {(True, True): "FizzBuzz",
@@ -14,36 +12,32 @@ def fizzbuzz(number=100):
              (False, True): "Buzz",
              (False, False): False }
     for x in range(1, number + 1):
-        test3 = str(float(x/3.0))
-        test5 = str(float(x/5.0))
-        t = (test_for_zero(test3), test_for_zero(test5))
+        t = (modulor(x, 3.0), modulor(x, 5.0))
         if words[t]:
             out.append(words[t])
         else:
             out.append(x)
     return out
 
-def test_for_zero(s):
+def modulor(a,b):
+    s = str(float(a/b))
     cut_period = s.replace('.', ' ')
     listed = cut_period.split()
     if int(listed[-1]) == 0:
         return True
     return False
 
- # validation of user input
-def check_for_int(number):
-    try:
-        int(number)
-        return True
-    except ValueError:
-        return False
 
 # loops until user supplies something that can be "intted"
-def get_integer(number):
+def validate_user_input(number):
     # test for integer loop
-    while check_for_int(number) == False:
-        number = raw_input("please enter a number; not letters or what not! > ")
-    return int(number)
+    while True:
+        try:
+            x = int(number)
+            return x
+        except ValueError:
+            number = raw_input("please enter a number; not letters or what not! > ")
+            pass
 
 # get initial user input
 def user_input():
@@ -53,7 +47,7 @@ def user_input():
 # main routine
 if __name__ == "__main__":
     number = user_input()
-    n = get_integer(number)
+    n = validate_user_input(number)
     the_list = fizzbuzz(n)
     for entry in the_list:
         print entry
@@ -69,4 +63,7 @@ if __name__ == "__main__":
             out.append(words[(modulor(x,3),modulor(x,5))])
         else:
             out.append(x)
+
+    while check_for_int(number) == False:
+        number = raw_input("please enter a number; not letters or what not! > ")
     """
--- tests.py
+++ tests.py
@@ -1,15 +1,15 @@
 """Create a separate file to test your code with unit tests. 
 Name this file *tests.py*. Write tests for each scenario (e.g, multiple of 3, 5, 15, etc.).
-testing for versions 9-10"""
+testing for version 13"""
 
 from random import randint
 import unittest
 import mock
-from fizzbuzz import fizzbuzz, user_input, get_integer, check_for_int
+from fizzbuzz import fizzbuzz, user_input, modulor, validate_user_input
 
 
 class FizzBuzzTests(unittest.TestCase):
-    """ testing Fizz Buzz """
+    """ testing fizzbuzz funtion """
 
     def test_threes(self):
         the_list = fizzbuzz()
@@ -42,28 +42,32 @@ class FizzBuzzTests(unittest.TestCase):
             number = index + 1
             if (number % 3) != 0 and (number % 5) != 0:
                 self.assertEqual(entry, number, msg="{} and {} should be equal integers".format(str(entry), str(number)))
+    
+    """ testing helper funtions """
 
     def test_user_input(self):
+        print "testing user_input"
         with mock.patch('__builtin__.raw_input', return_value=12):
             assert user_input() == 12
             print "user_input integer passed"
         with mock.patch('__builtin__.raw_input', return_value="frogs"):
             assert user_input() == "frogs"
             print "user_input string passed"
-
-    def test_get_integer(self):
-        print "testing get_integer"
-        with mock.patch('__builtin__.raw_input', return_value=24):
-            assert get_integer(24) == 24
-            print "get_integer integer passed"
-
-    def test_check_for_int(self):
-        test_list = (1, 44, "45", 'goat', "5dew3")
-        expected = [True, True, True, False, False]
+    
+    def test_modulor(self):
+        print "testing modulor"
+        test_list = [[9, 3.0], [13, 3.0], [5, 5.0], [31, 5.0], [15, 3.0], [15, 5.0]]
+        expected = [True, False, True, False, True, True]
         results = []
         for entry in test_list:
-            results.append(check_for_int(entry))
+            results.append(modulor(entry[0], entry[1]))
         self.assertEqual(expected, results)
+
+    def validate_user_input(self):
+        print "testing validate_user_input"
+        with mock.patch('__builtin__.raw_input', return_value=24):
+            assert get_integer(24) == 24
+            print "validate_user_input passed"
 
 if __name__ == "__main__":
     print "testing..."
